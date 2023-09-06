08c7733527edb4093eeef2ad3b02579743b05e95
bahaa26 <bahaaazoz398@gmail.com>
2023-09-06 05:16:37 +0300

    hot fix

--- resources/views/includes/modals.blade.php
+++ resources/views/includes/modals.blade.php
@@ -1,7 +1,5 @@
-@if(auth()->user()->hasRole('accountant'))
 @include('modals.department-report-query')
 @include('modals.department-daily-report-query')
 @include('modals.department-daily-report-in-total-query')
 @include('modals.general-settings-query')
 @include('modals.department-report-karat-difference-query')
-@endif
